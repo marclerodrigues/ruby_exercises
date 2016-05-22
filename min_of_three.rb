5c9f362567943333c769b7ea7dd5647d8ac00b51
Marcle Rodrigues <maarclee@gmail.com>
2016-05-22 16:22:13 -0300

    Max of Array

---
+++ min_of_three.rb
@@ -0,0 +1,21 @@
+data = IO.read('min_of_three_data.txt')
+data = data.split("\n")
+array = []
+min = []
+data.each do |pair|
+  array.push(pair.split(" "))
+end
+
+array.each do |pair|
+  for i in 0...pair.count
+    pair[i] = pair[i].to_f
+  end
+end
+
+array.each do |pair|
+  if pair.size >= 2
+    min.push(pair.min.to_i)
+  end
+end
+
+p min.join " "
